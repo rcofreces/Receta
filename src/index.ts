b43edd29ff39519d707916a982f4a75a7b88d5a3
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 14:31:24 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+//Hacer un Lemon Pie
+
+//Ingredientes para la masa
+let ingrediente1: string = "100 gramos de manteca";
+let ingrediente2: string = "1 yema de huevo";
+let ingrediente3: string = "2 cucharadas de azucar";
+let ingrediente4: string = "2 cucharadas de agua fria";
+let ingrediente5: string = "1 cucharada de vinagre";
+let ingrediente6: string = "1 taza de harina";
+
+//Ingredientes para el relleno
+let ingrediente7: string = "1 taza de agua";
+let ingrediente8: string = "1 taza de azucar";
+let ingrediente9: string = "2/3 de taza de jugo de limon";
+let ingrediente10: string = "2 cucharadas de fecula de maiz";
+let ingrediente11: string = "4 yemas de huevo";
+let ingrediente12: string = "4 cucharadas de manteca";
+let ingrediente13: string = "ralladura de 1 limon";
+
+//Ingredientes para el merengue italiano
+let ingrediente14: string = "4 claras de huevo";
+let ingrediente15: string = "200 gramos de azucar";
+
+console.log("Ejercicio 1.a");
+
+console.log(
+  "Paso 1: Mezclar para hacer la masa: " +
+    ingrediente1 +
+    " + " +
+    ingrediente2 +
+    " + " +
+    ingrediente3 +
+    " + " +
+    ingrediente4 +
+    " + " +
+    ingrediente5 +
+    " + " +
+    ingrediente6
+);
+console.log("Paso 2: Estirar la masa en un molde");
+console.log("Paso 3: Cocinar la masa y dejar enfriar");
+console.log(
+  "Paso 4: Hacer el relleno:" +
+    " Mezclar en un bowl" +
+    " " +
+    ingrediente7 +
+    " + " +
+    ingrediente8 +
+    " + " +
+    ingrediente9 +
+    " + " +
+    ingrediente10 +
+    "." +
+    " Cocinar a fuego lento hasta que hierva" +
+    " y agregar" +
+    " " +
+    ingrediente11
+);
+console.log(
+  "Paso 5: Retirar la mezcla del fuego y mientras este caliente mezclar con las" +
+    " " +
+    ingrediente12 +
+    " + " +
+    ingrediente13
+);
+console.log("Paso 6: Rellenar la masa con el relleno del paso 4");
+console.log(
+  "Paso 7: Hacer el merengue mezclando" +
+    " " +
+    ingrediente14 +
+    " " +
+    "y" +
+    " " +
+    ingrediente15
+);
